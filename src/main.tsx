343e20a0ee56108fae489e4c599183e167bc7d88
Yousef <yousefjavaherian@gmail.com>
2023-09-04 10:35:40 +0330

    fixing tanstack query issue

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import { NextUIProvider } from "@nextui-org/react";
 import App from "./App";
 import "./styles.css";
 import { Toaster } from 'sonner'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <NextUIProvider>
       <Toaster position="bottom-center" />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </NextUIProvider>
   </React.StrictMode>,
 );
